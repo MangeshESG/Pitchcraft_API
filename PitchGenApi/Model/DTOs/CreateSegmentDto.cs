192427f15836997d8aac6fb476d3c83cd50d2d2a
Aamir Shaikh <aamirs@groupji.co>
2025-07-28 18:01:41 +0530

    added sigment

---
+++ PitchGenApi/Model/DTOs/CreateSegmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PitchGenApi.Model.DTOs
+{
+    public class CreateSegmentDto
+    {
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public int? DataFileId { get; set; }
+        public List<int> ContactIds { get; set; } = new();
+    }
+
+}
