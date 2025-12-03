90cd49e51af2e4111ae490063380b104ac655154
Aamir Shaikh <aamirs@groupji.co>
2025-12-03 21:04:59 +0530

    Unsebscribe added with email handaling

---
+++ PitchGenApi/Model/UnsubscribedContacts.cs
@@ -0,0 +1,10 @@
+﻿namespace PitchGenApi.Model
+{
+    public class UnsubscribedContacts
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
